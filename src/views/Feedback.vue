3bcb651ad7471839a6c21973ac9ff4643d2150c5
Chris Violette <chris.violette@the-refinery.io>
2020-03-25 18:47:40 -0400

    more form testing

--- src/views/Feedback.vue
+++ src/views/Feedback.vue
@@ -21,7 +21,6 @@
           @submit.prevent="submit($event)"
           v-model="valid"
           netlify
-          data-netlify-recaptcha="true"
         >
           <input name="form-name" value="Quarantinio Feedback" type="hidden" />
           <v-card elevation="2" color="primary darken-1" dark shaped>
