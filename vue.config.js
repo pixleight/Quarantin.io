ba02d85086b6632d4a1694bf040792adcaadc9eb
Chris Violette <chris.violette@the-refinery.io>
2020-03-25 18:09:57 -0400

    tweaking vue config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,3 @@
-const path = require('path')
-const PrerenderSPAPlugin = require('prerender-spa-plugin')
-
 module.exports = {
   "transpileDependencies": [
     "vuetify"
@@ -13,22 +10,4 @@ module.exports = {
       background_color: '#1d333e'
     }
   },
-
-  configureWebpack: () => {
-    return {
-      plugins: [
-        new PrerenderSPAPlugin(
-          // Absolute path to compiled SPA
-          path.resolve(__dirname, 'dist'),
-          // List of routes to prerender
-          [
-            '/',
-          ],
-          {
-            //options
-          }
-        ),
-      ]
-    }
-  }
 }
