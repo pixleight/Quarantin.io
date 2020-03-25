150268586360e0cc102ab949b4a5b416a0850cad
Chris Violette <chris.violette@the-refinery.io>
2020-03-25 17:48:52 -0400

    setting up router history mode

--- src/router/index.js
+++ src/router/index.js
@@ -20,6 +20,7 @@ const routes = [
 ]
 
 const router = new VueRouter({
+  mode: 'history',
   routes
 })
 
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
+const path = require('path')
+const PrerenderSPAPlugin = require('prerender-spa-plugin')
+
 module.exports = {
   "transpileDependencies": [
     "vuetify"
@@ -10,4 +13,22 @@ module.exports = {
       background_color: '#1d333e'
     }
   },
+
+  configureWebpack: () => {
+    return {
+      plugins: [
+        new PrerenderSPAPlugin(
+          // Absolute path to compiled SPA
+          path.resolve(__dirname, 'dist'),
+          // List of routes to prerender
+          [
+            '/',
+          ],
+          {
+            //options
+          }
+        ),
+      ]
+    }
+  }
 }
