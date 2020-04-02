ca9f323560cbc69c8badad1f9c59abbb9dcf7e6a
Chris Violette <chris.violette@the-refinery.io>
2020-04-02 19:25:48 -0400

    bump to version 1.8.0

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,7 +7,7 @@ self.addEventListener('message', (event) => {
 });
 
 //Change this value every time before you build
-const LATEST_VERSION = 'v1.7.3'
+const LATEST_VERSION = 'v1.8.0'
 
 self.addEventListener('activate', (event) => {
   console.log(`%c ${LATEST_VERSION} `, 'background: #ddd; color: #0000ff')
